e98e1e10322cbb75556478196df6350ba93cd5fb
hkhanhdu <27678878+hkhanhdu@users.noreply.github.com>
2023-05-21 17:58:43 +0000

    next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'JAK1NG',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://github.com/hkhanhdu/k9',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
